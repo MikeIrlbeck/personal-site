904ae8a996aa66cea811a40589c7d23941846e59
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2023-03-25 11:01:46 -0600

    - for some strange reason, a className of 'social' works while 'social-links' would never show the items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,23 +6,25 @@ const Navbar = () => {
   return (
     <nav className='navbar'>
       <div className='nav-center'>
-        <Link to='/'>Mike Irlbeck{social[0].icon}</Link>
+        <Link to='/'>Mike Irlbeck</Link>
 
         <ul className='nav-links'>
           {links.map((link) => {
             const { id, url, text } = link;
             return (
-              <li id={id}>
+              <li key={id}>
                 <Link to={url}>{text}</Link>
               </li>
             );
           })}
         </ul>
-        <ul className='nav-links'>
+
+        <ul className='social'>
           {social.map((link) => {
             const { id, url, icon } = link;
+            console.log(link);
             return (
-              <li id={id}>
+              <li key={id}>
                 <Link to={url}>{icon}</Link>
               </li>
             );
