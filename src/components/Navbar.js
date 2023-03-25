68fa67ab08f68bd321e3f646c049c3aef6fe58d3
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2023-03-25 08:27:48 -0600

    added a basic structure and imported react router

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Error - why did you do that?</h1>
+    </div>
+  );
+};
+
+export default Error;
