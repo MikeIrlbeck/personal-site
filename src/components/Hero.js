1fcc7690dc77e3d72b17270f308f54da85868f93
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2023-03-25 12:26:03 -0600

    - added useRef hook to dynamically style the different images

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import hero from '../hero.jpeg';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 const Hero = () => {
   const [isPandaShowing, setIsPandaShowing] = useState(false);
+  const refImg = useRef(null);
+
+  useEffect(() => {
+    refImg.current.style.objectFit = isPandaShowing ? 'fill' : 'cover';
+  }, [isPandaShowing]);
 
   return (
     <>
@@ -15,6 +20,7 @@ const Hero = () => {
         </button>
         <img
           className='hero-img'
+          ref={refImg}
           src={
             isPandaShowing
               ? 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Faliljoy.com%2Fwp-content%2Fuploads%2F2012%2F07%2FBaby-panda-Wolong-Panda-Center-China.jpg&f=1&nofb=1&ipt=ba111961ee0415c2c1a399a8d677e0eb26108a6e5175486ee19e2cff479b2c7a&ipo=images'
