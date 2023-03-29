7c6a8f49ebf823acb0f4f7eb8e8fb53a8c066bfc
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2023-03-29 07:19:46 -0600

    - added some styling for single project page - added a video iframe if present as well github link and stars display

--- src/pages/SingleProject.js
+++ src/pages/SingleProject.js
@@ -5,13 +5,25 @@ import { useParams } from 'react-router-dom';
 const SingleProject = () => {
   const { id } = useParams();
   console.log('single project - ', id);
-  const { title, url, subTitle, stars, video } = data.find(
+  const { title, url, subTitle, stars, githubLink, video } = data.find(
     (item) => item.id === parseInt(id)
   );
   return (
     <div className='single-project'>
-      <h1>{title}</h1>
-      <h4>{subTitle}</h4>
+      <div className='single-project-container'>
+        <h1>{title}</h1>
+        <h4>{subTitle}</h4>
+        {video ? (
+          <iframe id='project-video' loading='lazy' src={video}></iframe>
+        ) : (
+          <img src={url} alt={`${title}`}></img>
+        )}
+        {stars && githubLink && (
+          <p>
+            <a href={githubLink}>Github. Stars {stars}</a>
+          </p>
+        )}
+      </div>
     </div>
   );
 };
@@ -21,6 +33,7 @@ SingleProject.propTypes = {
   url: PropTypes.string.isRequired,
   subTitle: PropTypes.string.isRequired,
   stars: PropTypes.number,
+  githubLink: PropTypes.string,
   video: PropTypes.string,
 };
 
