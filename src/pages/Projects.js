ab5250c9e7f819e9d286913fe0fbbd05d2068f6a
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2023-03-29 07:21:00 -0600

    - commented out links to allow a netlify build

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import data from '../data/Projects';
-import { links } from '../data/NavLinks';
+// import { links } from '../data/NavLinks';
 import { Link } from 'react-router-dom';
 
 const Projects = () => {
