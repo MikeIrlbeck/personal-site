08cd0de585cc681c44c4d08314fc01b2390e1f7a
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2023-04-06 07:07:42 -0600

    - made nav bar with toggle bars icon for better accessibility - inspired from John Smilga's sidebar project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { links } from '../data/NavLinks';
 
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      <div className='nav-center'>
-        <Link to='/'>Mike Irlbeck</Link>
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
-        <ul className='nav-links'>
-          {links.map((link) => {
-            const { id, url, text } = link;
-            return (
-              <li key={id}>
-                <Link to={url}>{text}</Link>
-              </li>
-            );
-          })}
-        </ul>
+  console.log(showLinks);
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <Link to='/'>
+            <span className='name'>Mike Irlbeck</span>
+          </Link>
+          {/* <img src={logo} alt='logo' /> */}
+          <button
+            className='nav-toggle'
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              // remember: 1. need to return one JSX component
+              // 2. when build an iteration of components, need an id
+              return (
+                <li key={id}>
+                  <Link to={url}>{text}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   );
